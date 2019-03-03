4fb7308a791a19b5efcb86d96a3d70d956052c5c
Sorochan Andriy <xai1983kbu@gmail.com>
2019-03-03 23:44:43 +0200

    commented event.preventDefault() in Fib.js

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -26,7 +26,7 @@ class Fib extends Component {
   }
 
   handleSubmit = async event => {
-    event.preventDefault();
+    // event.preventDefault();
 
     await axios.post('/api/values', {
       index: this.state.index
